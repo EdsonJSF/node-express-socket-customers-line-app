e42d27be0866f1363bb99c48aee35b05f929eb3d
EdsonJSF <edsonjsf1990@gmail.com>
2023-09-12 08:31:00 -0400

    Feat: Desk display

--- public/js/escritorio.js
+++ public/js/escritorio.js
@@ -1,3 +1,30 @@
+// HTML Ref
+const lblDesk = document.querySelector("h1");
+const buttonAttend = document.querySelector("button");
 
+const searchParams = new URLSearchParams(window.location.search);
 
-console.log('Escritorio HTML');
+if (!searchParams.has("escritorio")) {
+  window.location = "index.html";
+  throw new Error("Desk require");
+}
+
+const desk = searchParams.get("escritorio");
+lblDesk.innerText = desk;
+
+const socketClient = io();
+
+socketClient.on("connect", () => {
+  buttonAttend.disabled = false;
+});
+
+socketClient.on("disconnect", () => {
+  buttonAttend.disabled = true;
+});
+
+socketClient.on("last-ticket", (lastTicket) => {});
+
+buttonAttend.addEventListener("click", () => {
+  // socketClient.emit("", null, (ticket) => {
+  // });
+});
